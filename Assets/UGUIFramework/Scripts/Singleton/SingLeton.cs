94feb35c9b982eeba16ba77597e5c44b846414c8
Mumu <positivemumu@126.com>
2021-05-22 23:13:58 +0800

    三、UGI框架 1. 旧版UI框架添加

---
+++ Assets/UGUIFramework/Scripts/Singleton/SingLeton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/// <summary>
+/// 单例模板类,不继承自monobehavior的单例类可继承该类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingLeton<T> where T : class, new()
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new T();
+            return instance;
+        }
+    }
+}
---
+++ Assets/UGUIFramework/Scripts/UI/Base/PanelInfo.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelInfo
+{
+
+    #region 数据成员
+    //包名
+    private string _panelName;
+    public string PanelName
+    {
+        get
+        {
+            return _panelName;
+        }
+
+        set
+        {
+            _panelName = value;
+        }
+    }
+    //窗体名
+    private string _panelPath;
+    public string PanelPath
+    {
+        get
+        {
+            return _panelPath;
+        }
+
+        set
+        {
+            _panelPath = value;
+        }
+    }
+    //window类型
+    private UIPanelTypes _uiPanelType;
+    public UIPanelTypes UIPanelType
+    {
+        get
+        {
+            return _uiPanelType;
+        }
+        set
+        {
+            _uiPanelType = value;
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// 获取当前Panel的名字
+    /// </summary>
+    /// <returns></returns>
+    public string GetPanelName()
+    {
+        return _panelName;
+    }
+
+    /// <summary>
+    /// 获取当前Panel的加载路径
+    /// </summary>
+    /// <returns></returns>
+    public string GetPanelPath()
+    {
+        return _panelPath;
+    }
+}
